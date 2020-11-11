336c23bcb3c4cc404139b97dbc8228717edb3a42
maham <mahamshamail@gmail.com>
2020-11-11 18:00:32 -0500

    Initial Commit

---
+++ FinalExam_Maham_101328732/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FinalExam_Maham_101328732
+//
+//  Created by Maham Shamail on 11/11/2020.
+//  Copyright © 2020 Maham Shamail. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
